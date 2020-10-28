c99cd94242e772d7d25694f54ee8eccdadbc3e55
InterestingBrainPoops <satasiyahet@gmail.com>
2020-10-27 19:02:19 -0700

    Init Commit

---
+++ ButtonPress.py
@@ -0,0 +1,1 @@
+print("A button got pressed")
---
+++ index.js
@@ -0,0 +1,32 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var fs = require('fs');
+const io = require('socket.io')(http);
+const { exec } = require("child_process");
+app.get('/', (req, res) => {
+  //res.writeHead(200);
+  let html = fs.readFileSync("./index.html", "utf-8");
+  res.write(html);
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+io.on('connect', socket => {
+  console.log(socket.id);
+  socket.on('pressed', ()=>{
+    console.log("press");
+    exec("python ButtonPress.py", (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+
+    });
+  });
+});
